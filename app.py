de5a05ea3ef1bc339cb071916c71c956b256beac
ankkarp <anna.a.k.2002@gmail.com>
2023-03-16 23:51:34 +0300

    classes init and requirements

---
+++ app.py
@@ -0,0 +1,6 @@
+import argparse
+
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--input_path', '-i', help='Путь к входному файлу')
+args = parser.parse_args()
---
+++ src/checkup.py
@@ -0,0 +1,1 @@
+# class DomainCheckup:
